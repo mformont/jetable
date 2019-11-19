0179a1c8f90ee93a42603478405af87a82c48d72
formont <mickael.formont@trsb.net>
2019-11-19 10:32:40 +0100

    test pour la brache

---
+++ unittests/TestTest.java
@@ -0,0 +1,20 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.jalios.jcms.test.JcmsTestCase4;
+
+
+public class TestTest extends JcmsTestCase4 {
+	
+	@Test
+	public void First() {
+		assertTrue(false);
+	}
+	
+	@Test
+	public void First() {
+		assertTrue(true);
+	}
+
+}
